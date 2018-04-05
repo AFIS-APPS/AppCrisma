52de6d2056b6f76a732b5847ac371d010abd9c8c
AFIS-APPS <afis.apps@gmail.com>
2018-04-04 21:33:02 -0300

    Alteração nas Classes

--- app/src/main/java/com/appcrisma/afi/appcrisma/Helper/BDContas.java
+++ app/src/main/java/com/appcrisma/afi/appcrisma/Helper/BDContas.java
@@ -1,4 +1,26 @@
 package com.appcrisma.afi.appcrisma.Helper;
 
 public class BDContas {
+
+    String chaveId, nomeUser;
+
+    public BDContas(){
+
+    }
+
+    public String getChaveId() {
+        return chaveId;
+    }
+
+    public void setChaveId(String chaveId) {
+        this.chaveId = chaveId;
+    }
+
+    public String getNomeUser() {
+        return nomeUser;
+    }
+
+    public void setNomeUser(String nomeUser) {
+        this.nomeUser = nomeUser;
+    }
 }
