2ad7044a1a0097e1136180c7440ab9a28f544b1e
AFIS-APPS <afis.apps@gmail.com>
2018-04-12 18:22:19 -0300

    Lista Personalisada 2

--- app/src/main/java/com/appcrisma/afi/appcrisma/Helper/ListaAdapter.java
+++ app/src/main/java/com/appcrisma/afi/appcrisma/Helper/ListaAdapter.java
@@ -1,10 +1,10 @@
 package com.appcrisma.afi.appcrisma.Helper;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,24 +14,40 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.appcrisma.afi.appcrisma.Configs.FirebaseConfig;
 import com.appcrisma.afi.appcrisma.Models.RegFrequencia;
 import com.appcrisma.afi.appcrisma.Models.Turmas;
 import com.appcrisma.afi.appcrisma.R;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class ListaAdapter extends ArrayAdapter<Turmas>{
         private ArrayList<Turmas> turmas;
         private Context context;
-        private RegFrequencia frequencia;
+        private RegFrequencia[] frequencia;
+        private int count;
+        private Stack<RegFrequencia> stackFreq;
 
         public ListaAdapter(Context c, ArrayList<Turmas> objects) {
             super(c, 0, objects);
             this.turmas = objects;
             this.context = c;
+            stackFreq = new Stack<>();
+            frequencia = new RegFrequencia[2];
         }
 
-        @Override
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Override
+    public int getPosition(@Nullable Turmas item) {
+        return super.getPosition(item);
+    }
+
+    @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
             View view = null;
@@ -52,28 +68,37 @@ public class ListaAdapter extends ArrayAdapter<Turmas>{
                 final CheckBox chkFaltou = (CheckBox) view.findViewById(R.id.checkBoxFaltou);
 
                 //monta a exibição de acordo com os dados passados
-                Turmas turma = turmas.get( position );
+                final Turmas turma = turmas.get( position );
+
                 nome.setText( turma.getNomeCrismando().toUpperCase());
                 nfaltas.setText(String.valueOf(turma.getNumFaltas()));
 
                 //Controle de registro de frequencia
-                frequencia = new RegFrequencia();
-                frequencia.setNomeCrismando(turma.getNomeCrismando());
+                count = getPosition(turma);
+                frequencia[count] = new RegFrequencia();
+                frequencia[count].setNomeCrismando(turma.getNomeCrismando());
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    chkFaltou.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#40e716")));
-                    chkPresente.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#ff0000")));
+                    chkFaltou.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#ff0000")));
+                    chkPresente.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#40e716")));
                 }
                 chkPresente.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                         if(isChecked){
-                            frequencia.setPresente(true);
-                            Toast.makeText(context, "O Aluno " + frequencia.getNomeCrismando()+" veio", Toast.LENGTH_SHORT).show();
+                            count = getPosition(turma);
+                            frequencia[count].setPresente(true);
+                            Toast.makeText(context, "O Aluno " + frequencia[count].getNomeCrismando()+" veio", Toast.LENGTH_SHORT).show();
+
+                                if(stackFreq.search(frequencia[count])  == 1){
+                                    stackFreq.pop();
+                                    stackFreq.push(frequencia[count]);
+                                }else{
+                                    stackFreq.push(frequencia[count]);
+                                }
                             chkFaltou.setChecked(false);
-                        }else {
-                            chkFaltou.setChecked(true);
+
                         }
                     }
                 });
@@ -82,23 +107,39 @@ public class ListaAdapter extends ArrayAdapter<Turmas>{
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                         if(isChecked){
-                           frequencia.setPresente(false);
-                            Toast.makeText(context, "O Aluno " + frequencia.getNomeCrismando()+" faltou", Toast.LENGTH_SHORT).show();
+                           count = getPosition(turma);
+                           frequencia[count].setPresente(false);
+                            Toast.makeText(context, "O Aluno " + frequencia[count].getNomeCrismando()+" faltou", Toast.LENGTH_SHORT).show();
+
+                            if(stackFreq.search(frequencia[count]) == 1){
+                                stackFreq.pop();
+                                stackFreq.push(frequencia[count]);
+                            }else{
+                                stackFreq.push(frequencia[count]);
+                            }
                             chkPresente.setChecked(false);
-                        }else {
-                            chkPresente.setChecked(true);
                         }
                     }
                 });
-
-
-
             }
             return view;
         }
 
-    public RegFrequencia getFrequencia() {
-        return frequencia;
-    }
+        public void salvarFirebase(Stack<RegFrequencia> stack){
+            while(!stack.empty()){
+                frequencia[count] = stack.peek();
+
+                FirebaseConfig.getDatabaseReference().child(frequencia[count].getDataRegistro()).child(frequencia[count].getTurma())
+                        .child(frequencia[count].getNomeCrismando()).setValue(frequencia[count]);
+
+
+
+                count--;
+                stack.pop();
+
+            }
+
+        }
+
 }
 
